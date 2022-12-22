46ec599588d21e804459aa9c3ffa566023fbfd9e
pumb3r <mishakvochko@ukr.net>
2022-12-22 14:04:18 +0200

    adds some fix

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ import { handleValidationErrors, checkAuth } from './utils/index.js';
 import { UserController, PostController } from './controllers/index.js';
 
 mongoose
-   .connect('mongodb+srv://heilrake:heilrake@cluster0.r85yakw.mongodb.net/?retryWrites=true&w=majority')
+   .connect(procces.env.MONGODB_URI)
    .then(() => console.log('DB ok'))
    .catch((err) => console.log('DB error', err));
 
