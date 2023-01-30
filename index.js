1f75cff4a54b3a661f0320d66c55b5eeb449b121
hailrake <mishakvochko@ukr.net>
2023-01-30 10:49:19 +0200

    hotfix

--- index.js
+++ index.js
@@ -12,22 +12,24 @@ import { handleValidationErrors, checkAuth } from './utils/index.js';
 import { UserController, PostController } from './controllers/index.js';
 
 mongoose
-   .connect(procces.env.MONGODB_URI)
-   .then(() => console.log('DB ok'))
-   .catch((err) => console.log('DB error', err));
+  .connect(
+    'mongodb+srv://heilrake:380638132867Qq@cluster0.ycsxapa.mongodb.net/?retryWrites=true&w=majority',
+  )
+  .then(() => console.log('DB ok'))
+  .catch((err) => console.log('DB error', err));
 
 const app = express();
 
 const storage = multer.diskStorage({
-   destination: (_, __, cb) => {
-      if (!fs.existsSync('uploads')) {
-         fs.mkdirSync('uploads');
-      }
-      cb(null, 'uploads');
-   },
-   filename: (_, file, cb) => {
-      cb(null, file.originalname);
-   },
+  destination: (_, __, cb) => {
+    if (!fs.existsSync('uploads')) {
+      fs.mkdirSync('uploads');
+    }
+    cb(null, 'uploads');
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
 const upload = multer({ storage });
@@ -41,9 +43,9 @@ app.post('/auth/register', registerValidation, handleValidationErrors, UserContr
 app.get('/auth/me', checkAuth, UserController.getMe);
 
 app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
-   res.json({
-      url: `/uploads/${req.file.originalname}`,
-   });
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
 });
 
 app.get('/tags', PostController.getLastTags);
@@ -54,17 +56,17 @@ app.get('/posts/:id', PostController.getOne);
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
 app.patch(
-   '/posts/:id',
-   checkAuth,
-   postCreateValidation,
-   handleValidationErrors,
-   PostController.update,
+  '/posts/:id',
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update,
 );
 
 app.listen(process.env.PORT || 4444, (err) => {
-   if (err) {
-      return console.log(err);
-   }
+  if (err) {
+    return console.log(err);
+  }
 
-   console.log('Server OK');
+  console.log('Server OK');
 });
